83cb395d1bdc49256fa2fa49eee92237c4501f3d
Anil Kallamkatil Balakrishnan <anil.balakrishnan@shortcut.no>
2020-06-12 22:03:28 +0200

    Created project and aded weather icons

---
+++ weather/weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Anil Kallamkatil Balakrishnan on 12/06/2020.
+//  Copyright © 2020 nrk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
