86ed4f04d1ee683c3776f3a95b037f54dc101b12
Anil Kallamkatil Balakrishnan <anil.balakrishnan@shortcut.no>
2020-06-13 14:58:55 +0200

    Added api constant file

---
+++ weather/weather/Util/WeatherApiConstant.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherApiConstant.swift
+//  weather
+//
+//  Created by Anil Kallamkatil Balakrishnan on 13/06/2020.
+//  Copyright © 2020 nrk. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherApiConstant {
+    static let baseUrl: String = "https://apitest.vipps.no"
+}
