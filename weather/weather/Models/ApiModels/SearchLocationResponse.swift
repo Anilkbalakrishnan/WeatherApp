671827640f703989f9592194ac03402eb64f6558
Anil Kallamkatil Balakrishnan <anil.balakrishnan@shortcut.no>
2020-06-13 14:58:27 +0200

    Added Network request data models

---
+++ weather/weather/Models/ApiModels/SearchLocationResponse.swift
@@ -0,0 +1,94 @@
+//
+//  SearchLocationResponse.swift
+//  weather
+//
+//  Created by Anil Kallamkatil Balakrishnan on 13/06/2020.
+//  Copyright © 2020 nrk. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - SearchLocationResponse
+struct SearchLocationResponse: Decodable {
+    let totalResults: Int
+    let links: Links
+    let embedded: Embedded
+
+    enum CodingKeys: String, CodingKey {
+        case totalResults
+        case links = "_links"
+        case embedded = "_embedded"
+    }
+}
+
+// MARK: - Embedded
+struct Embedded: Decodable {
+    let location: [Location]
+}
+
+// MARK: - Location
+struct Location: Decodable {
+    let category: Category
+    let id, name: String
+    let position: Position
+    let elevation: Int
+    let timeZone: TimeZone
+    let urlPath: String
+    let country, region, subregion: Category
+    let links: LocationLinks
+
+    enum CodingKeys: String, CodingKey {
+        case category, id, name, position, elevation, timeZone, urlPath, country, region, subregion
+        case links
+    }
+}
+
+// MARK: - Category
+struct Category: Decodable {
+    let id, name: String
+}
+
+// MARK: - LocationLinks
+struct LocationLinks: Decodable {
+    let linksSelf, celestialevents, forecast, mapfeature: Reference
+    let notifications, extremeforecasts, now: Reference
+    let observations: [Reference]
+    let airqualityforecast, pollen, bathingtemperatures: Reference
+
+    enum CodingKeys: String, CodingKey {
+        case linksSelf
+        case celestialevents, forecast, mapfeature, notifications, extremeforecasts, now, observations, airqualityforecast, pollen, bathingtemperatures
+    }
+}
+
+// MARK: - SelfElement
+struct Reference: Decodable {
+    let href: String
+}
+
+// MARK: - Position
+struct Position: Decodable {
+    let lat, lon: Double
+}
+
+enum TimeZone: String, Codable {
+    case europeOslo = "Europe/Oslo"
+}
+
+// MARK: - Links
+struct Links: Decodable {
+    let linksSelf: Reference
+    let page, search: Page
+    let location: [Reference]
+
+    enum CodingKeys: String, CodingKey {
+        case linksSelf = "self"
+        case page, search, location
+    }
+}
+
+// MARK: - Page
+struct Page: Decodable {
+    let href: String
+    let templated: Bool
+}
---
+++ weather/weather/Models/ApiModels/WeatherForcastResponse.swift
@@ -0,0 +1,96 @@
+//
+//  WeatherForcastResponse.swift
+//  weather
+//
+//  Created by Anil Kallamkatil Balakrishnan on 13/06/2020.
+//  Copyright © 2020 nrk. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct WeatherForcastResponse: Decodable {
+    let created, update: Date
+    let shortIntervals: [ShortInterval]
+    let longIntervals: [LongInterval]
+    let links: Link
+
+    enum CodingKeys: String, CodingKey {
+        case created, update, shortIntervals, longIntervals
+        case links = "_links"
+    }
+}
+
+// MARK: - Links
+struct Link: Decodable {
+    let linksSelf: Reference
+    enum CodingKeys: String, CodingKey {
+        case linksSelf
+    }
+}
+
+// MARK: - LongInterval
+struct LongInterval: Decodable {
+    let symbol: Symbol
+    let precipitation, temperature: Precipitation
+    let wind: Wind
+    let feelsLike, pressure: DewPoint
+    let cloudCover: CloudCover
+    let humidity, dewPoint: DewPoint
+    let start, end: Date
+    let uvIndex: DewPoint?
+}
+
+// MARK: - CloudCover
+struct CloudCover: Decodable {
+    let value, high, middle, low: Int
+    let fog: Int
+}
+
+// MARK: - DewPoint
+struct DewPoint: Decodable {
+    let value: Double?
+}
+
+// MARK: - Precipitation
+struct Precipitation: Decodable {
+    let min, max, value: Double
+    let pop: Int?
+}
+
+// MARK: - Symbol
+struct Symbol: Decodable {
+    let sunup: Bool
+    let n, clouds, precip: Int
+    let symbolVar: Var
+
+    enum CodingKeys: String, CodingKey {
+        case sunup, n, clouds, precip
+        case symbolVar
+    }
+}
+
+enum Var: String, Decodable {
+    case moon = "Moon"
+    case sun = "Sun"
+}
+
+// MARK: - Wind
+struct Wind: Decodable {
+    let direction: Int
+    let gust: Double?
+    let speed: Double
+}
+
+// MARK: - ShortInterval
+struct ShortInterval: Decodable {
+    let symbol: Symbol
+    let precipitation: Precipitation
+    let temperature: DewPoint
+    let wind: Wind
+    let feelsLike, pressure, uvIndex: DewPoint
+    let cloudCover: CloudCover
+    let humidity, dewPoint: DewPoint
+    let start, end: Date
+}
+
