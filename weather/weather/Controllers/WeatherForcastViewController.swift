90d5630374ac90f697d816958f529f324b0b4647
Anil Kallamkatil Balakrishnan <anil.balakrishnan@shortcut.no>
2020-06-13 14:57:20 +0200

    create WeatherForcastViewController

---
+++ weather/weather/Controllers/WeatherForcastViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherForcastViewController.swift
+//  weather
+//
+//  Created by Anil Kallamkatil Balakrishnan on 13/06/2020.
+//  Copyright © 2020 nrk. All rights reserved.
+//
+
+import UIKit
+
+class WeatherForcastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
