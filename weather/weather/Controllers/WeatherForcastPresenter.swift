61bf36dc1967976a822191204e917913e9d31cdc
Anil Kallamkatil Balakrishnan <anil.balakrishnan@shortcut.no>
2020-06-14 10:58:50 +0200

    init code for presenter

--- weather/weather/Controllers/WeatherForcastPresenter.swift
+++ weather/weather/Controllers/WeatherForcastPresenter.swift
@@ -10,4 +10,26 @@ import Foundation
 
 class WeatherForcastPresenter {
     
+    private let weatherService: WeatherServiceForcastInterface
+    weak private var viewDelegate: WeatherForcastViewControllerDelegate?
+    
+    init(weatherService: WeatherServiceForcastInterface) {
+        self.weatherService = weatherService
+        self.weatherService.setDataDelegate(delegate: self)
+    }
+    
+}
+
+//MARK:- Weather Forcast data delegeate
+extension WeatherForcastPresenter: WeatherServiceForcastDataDelegate {
+    
+    func onWeatherForcastFetchDidSucceed(weatherForcastResponse: WeatherForcastResponse) {
+        
+    }
+    
+    func onWeatherForcastFetchDidFiled() {
+        
+    }
+    
+    
 }
