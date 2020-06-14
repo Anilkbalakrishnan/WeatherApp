abdd95b152cd139660e37635ab65b1904b66d84f
Anil Kallamkatil Balakrishnan <anil.balakrishnan@shortcut.no>
2020-06-14 15:26:51 +0200

    Added an extension method to get the weather iconName from Symbol

--- weather/weather/Extension/WeatherApiModelExtension.swift
+++ weather/weather/Extension/WeatherApiModelExtension.swift
@@ -19,7 +19,7 @@ extension WeatherForcastResponse {
         let forcastGroupedByDate = Dictionary(grouping: self.shortIntervals) { $0.start.getDate }
         var forcasts = [Forcast]()
         for (date, intervals) in forcastGroupedByDate {
-            let forcast = Forcast(day: date, date: date, dayWeatherIcon: "02d", maxTemp: "21°", minTemp: "22°", currentTemp: "22°", weatherPrediction: intervals.map{$0.toWeatherInfo})
+            let forcast = Forcast(forcastSummary: ForcastSummary(day: date, date: date, dayWeatherIcon: "02d", maxTemp: "21°", minTemp: "22°", currentTemp: "22°"), weatherPrediction: intervals.map{$0.toWeatherInfo})
             forcasts.append(forcast)
         }
         
@@ -30,10 +30,31 @@ extension WeatherForcastResponse {
 
 extension ShortInterval {
     var toWeatherInfo: WeatherInfo {
-        return WeatherInfo(temperature: String(Int(self.temperature.value ?? 0)) + "°" , time: self.start.getTime, iconName: "02d")
+        return WeatherInfo(temperature: String(Int(self.temperature.value ?? 0)) + "°" , time: self.start.getTime, iconName: self.symbol.getIconName)
     }
 }
 
+//MARK:- Symbol Extension
+
+extension Symbol {
+    var getIconName: String {
+        var iconName = self.n > 9 ? String(self.n) : "0" + String(self.n)
+        switch self.symbolVar {
+        case "Moon":
+            iconName = iconName + "n"
+        case "Sun":
+            iconName = iconName + "d"
+//TODO:- Need to find what is the value for Polar night and add a case insetead of in deafult
+        default:
+            iconName + "m"
+        }
+        return iconName
+    }
+}
+
+
+
+//MARK:- Date Extesnison
 extension Date {
     var getTime: String {
         let df = DateFormatter()
